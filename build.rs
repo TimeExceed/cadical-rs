bd0f5ca74abbfdde04d959569f98d73a9ab68503
Taoda <tyf00@aliyun.com>
2025-05-06 13:27:35 +0800

    builds with libc++, musl-libc and clang

--- build.rs
+++ build.rs
@@ -1,50 +1,69 @@
 #![feature(exit_status_error)]
 
-use cmake::Config;
-use giputils::build::copy_build;
 use std::env;
-use std::path::PathBuf;
-use std::process::Command;
+use std::fs;
+use std::path::{PathBuf, Path};
 
 fn main() -> Result<(), String> {
     giputils::build::git_submodule_update()?;
-    let out_dir = env::var("OUT_DIR").unwrap();
 
-    let bindings = PathBuf::from("./bindings");
     println!("cargo:rerun-if-changed=./bindings");
-    Config::new(bindings).build();
-
     println!("cargo:rerun-if-changed=./cadical");
-    let cb_path = copy_build("cadical", |src| {
-        Command::new("sh")
-            .arg("configure")
-            .current_dir(src)
-            .status()
-            .map_err(|e| e.to_string())?
-            .exit_ok()
-            .map_err(|e| e.to_string())?;
-        let num_jobs = env::var("NUM_JOBS").unwrap();
-        Command::new("make")
-            .arg(format!("-j{}", num_jobs))
-            .current_dir(src)
-            .status()
-            .map_err(|e| e.to_string())?
-            .exit_ok()
-            .map_err(|e| e.to_string())
-    })?;
-    println!(
-        "cargo:rustc-link-search=native={}",
-        cb_path.join("build").display()
-    );
-    println!(
-        "cargo:rustc-link-search=native={}",
-        PathBuf::from(out_dir).join("lib").display()
-    );
+    let cadical_cpps = collect_cpps(&[&Path::new("cadical/src")]);
+    let mut build = cc::Build::new();
+    build.files(cadical_cpps)
+        .file("cadical/contrib/craigtracer.cpp")
+        .file("bindings/binding.cpp")
+        .includes([
+            "cadical/src",
+            "cadical/contrib",
+            "bindings",
+            "/usr/include/c++/v1",
+            "/usr/include/",
+        ])
+        .no_default_flags(true)
+        .define("NCLOSEFROM", "1")
+        .cpp(true)
+        .cpp_link_stdlib(None)
+        .warnings(false)
+        .opt_level(3)
+        .std("c++17")
+        .static_flag(true)
+        .shared_flag(false)
+        .flag("-nostdinc")
+        .flag("-nostdlib")
+        .flag("-Wno-shift-op-parentheses");
+    if let Ok(target) = std::env::var("TARGET") {
+        if target.contains("musl") {
+            if let Ok(wrapper) = std::env::var("RUST_WRAPPER") {
+                if wrapper.contains("sccache") {
+                    unsafe {std::env::set_var("CXX", "sccache clang++")};
+                }
+            }
+        }
+    }
+    build.compile("cadical");
+
+    let out_dir = &env::var("OUT_DIR").unwrap();
+    let out_dir = Path::new(out_dir);
+    println!("cargo:rustc-link-search=native={}", out_dir.display());
     println!("cargo:rustc-link-lib=static=cadical");
-    println!("cargo:rustc-link-lib=static=bindings");
-    #[cfg(target_os = "linux")]
-    println!("cargo:rustc-link-lib=dylib=stdc++");
-    #[cfg(target_os = "macos")]
-    println!("cargo:rustc-link-lib=dylib=c++");
     Ok(())
+}
+
+fn collect_cpps(verific_dirs: &[&Path]) -> Vec<PathBuf> {
+    let mut cpps = vec![];
+    for dir in verific_dirs.iter() {
+        for entry in fs::read_dir(dir).unwrap() {
+            let entry = entry.unwrap();
+            let p = entry.path().canonicalize().unwrap();
+            match p.extension().map(|x| x.to_str().unwrap()) {
+                Some("cpp") => {
+                    cpps.push(p);
+                }
+                _ => {}
+            }
+        }
+    }
+    cpps
 }
